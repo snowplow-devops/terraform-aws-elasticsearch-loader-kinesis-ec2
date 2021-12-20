ebde374a7a8a130f9398d568906b564932f3342b
Paul Boocock <paul@snowplowanalytics.com>
2021-12-20 15:00:38 +0000

    Prepared for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "elasticsearch-loader-kinesis-ec2"
-  module_version = "0.1.1"
+  module_version = "0.1.2"
 
   app_name    = "snowplow-elasticsearch-loader"
   app_version = "1.0.4"
